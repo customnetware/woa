c6c2333b0a74d62c12b19184a942b5bf19b1ce94
customnetware <138068712+customnetware@users.noreply.github.com>
2023-06-29 02:46:08 -0700

    Update woa_resident_home.js

--- woa_resident_home.js
+++ woa_resident_home.js
@@ -1,11 +1,11 @@
 
 
 document.getElementById("overlay").addEventListener("click", function () { document.getElementById("overlay").style.display = "none"; }, false);
-var loginStatus = document.getElementById("HeaderPublishAuthLogout");
-if (loginStatus !== null) { loginStatus.href = "https://ourwoodbridge.net/page/28118~1094081/logging-out" };
+    var loginStatus = document.getElementById("HeaderPublishAuthLogout");
+    if (loginStatus !== null) { loginStatus.href = "https://ourwoodbridge.net/page/28118~1094081/logging-out" };
 
-var m_loginStatus = document.getElementById("head-mobile").getElementsByClassName("mobile-menu-word-link");
-if (m_loginStatus !== null && m_loginStatus.length == 2) { m_loginStatus[1].href = "https://ourwoodbridge.net/page/28118~1094081/logging-out" };
+    var m_loginStatus = document.getElementById("head-mobile").getElementsByClassName("mobile-menu-word-link");
+    if (m_loginStatus !== null && m_loginStatus.length == 2) { m_loginStatus[1].href = "https://ourwoodbridge.net/page/28118~1094081/logging-out" };
 
 var emailDiv = document.getElementById("emailList");
 var docDiv = document.getElementById("docList");
@@ -19,13 +19,13 @@ var woaFrame = document.getElementById("MyFrame");
 var inter = window.setInterval(function () {
     if (woaFrame.contentWindow.document.readyState === "complete") {
         window.clearInterval(inter);
+        alert("loaded")
         woaFrame.contentWindow.addEventListener('load', memberProfile)
-
-
     }
 }, 100)
 function memberProfile() {
-        document.getElementById("overlay").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
+
     var woaPage = woaFrame.contentWindow.document
     if (profileTitle.getElementsByTagName("a").length > 0) {
         profileTitle.getElementsByTagName("a")[0].innerText = woaPage.getElementsByClassName("clsHeader")[0].innerText
@@ -113,7 +113,7 @@ function memberProfile() {
         recentsellsUL.appendChild(contentLI);
     }
     sellDiv.appendChild(recentsellsUL);
-  
+
 }
 
 
