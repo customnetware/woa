4baa72b7e0858be5be028bdf5e21d95b1870655f
customnetware <138068712+customnetware@users.noreply.github.com>
2023-06-29 21:22:57 -0700

    Update woa_resident_home.js

--- woa_resident_home.js
+++ woa_resident_home.js
@@ -1,12 +1,10 @@
-window.onload = function () {
+
+
 var p_name = document.getElementById("profile_name")
 var p_address = document.getElementById("profile_address")
 var p_city = document.getElementById("profile_city")
 var p_image = document.getElementById("profile_image")
-p_name.innerText = "Resident's Name"
-p_address.innerText = "Resident's Address"
-p_city.innerText = "Resident City"
-p_image.src = "https://customnetware.github.io/woa/profile_avatar.png"
+
 
 document.getElementById("overlay").addEventListener("click", function () { document.getElementById("overlay").style.display = "none"; }, false);
 
@@ -127,4 +125,3 @@ woaFrame.onload = function () {
 
 
 
-}
