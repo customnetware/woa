6073783ea6d923f24039fc63afcaee06233cfaa4
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-06 05:55:59 -0700

    Update new_resident_home.js

--- new_resident_home.js
+++ new_resident_home.js
@@ -2,8 +2,9 @@ var woaFrame = document.getElementById("MyFrame");
 var profileData = document.getElementById("profile_data").getElementsByClassName("card-body");
 const page_content = ["panel_news_content", "panel_messages_content", "panel_classifieds_content", "panel_resource_content", "panel_cal_content", "panel_discuss_content"];
 const content_class = ["news", "message", "classified", "document", "event", "discussion"];
+const content_index = [0,1,2,4,5,6];
 for (let i = 0; i < page_content.length; i++) {
-    checkContent(page_content[i], content_class[i], i)
+    checkContent(page_content[i], content_class[i], content_index[i])
 }
 
 function checkContent(contentToCheck, classToCheck, contentDivNum) {
