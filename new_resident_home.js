744b28fb4588ac42aa2eec3f0332de5e4eba58db
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-06 05:07:17 -0700

    Update new_resident_home.js

--- new_resident_home.js
+++ new_resident_home.js
@@ -1,12 +1,12 @@
 var woaFrame = document.getElementById("MyFrame");
 const page_content = ["panel_news_content", "panel_messages_content", "panel_classifieds_content", "panel_resource_content", "panel_cal_content", "panel_discuss_content"];
 for (let i = 0; i < page_content.length; i++) {
-    checkContent(current_content)
+    checkContent(page_content[i])
 }
 
 function checkContent(contentToCheck) {
     var page_wait = window.setInterval(function () {
-        let current_content = woaFrame.contentWindow.document.getElementById(contentToCheck[i]);
+        let current_content = woaFrame.contentWindow.document.getElementById(contentToCheck);
         if (current_content !== null) {
             alert(current_content.innerText)
             window.clearInterval(page_wait);
