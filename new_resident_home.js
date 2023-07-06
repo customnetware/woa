681f2f7d096b77307aca642237e17bee001fac65
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-06 05:39:33 -0700

    Update new_resident_home.js

--- new_resident_home.js
+++ new_resident_home.js
@@ -2,16 +2,20 @@ var woaFrame = document.getElementById("MyFrame");
 const page_content = ["panel_news_content", "panel_messages_content", "panel_classifieds_content", "panel_resource_content", "panel_cal_content", "panel_discuss_content"];
 const content_class = ["news", "message", "classified", "document", "event", "discussion"];
 for (let i = 0; i < page_content.length; i++) {
-    checkContent(page_content[i], content_class[i])
+    checkContent(page_content[i], content_class[i],i)
 }
 
-function checkContent(contentToCheck,classToCheck) {
+function checkContent(contentToCheck, classToCheck,contentDivNum) {
     var page_wait = window.setInterval(function () {
         let current_content = woaFrame.contentWindow.document.getElementById(contentToCheck);
         if (current_content !== null) {
-            let current_content_class = current_content.getElementsByClassName(classToCheck)
-            alert(current_content_class[0].innerText)
             window.clearInterval(page_wait);
+            let current_content_class = current_content.getElementsByClassName(classToCheck)
+            let contentUL = document.createElement('ul');
+            let contentLI = document.createElement('li');
+            contentLI.innerHTML = current_content_class[0]
+            contentUL.appendChild(contentLI);
+            profileData[contentDivNum].appendChild(contentUL)
         }
     }, 50)
 }
