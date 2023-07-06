6c05f5ff05cf8a6f7cdcf347084aa235d6dc41aa
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-06 05:48:55 -0700

    Update new_resident_home.js

--- new_resident_home.js
+++ new_resident_home.js
@@ -3,19 +3,22 @@ var profileData = document.getElementById("profile_data").getElementsByClassName
 const page_content = ["panel_news_content", "panel_messages_content", "panel_classifieds_content", "panel_resource_content", "panel_cal_content", "panel_discuss_content"];
 const content_class = ["news", "message", "classified", "document", "event", "discussion"];
 for (let i = 0; i < page_content.length; i++) {
-    checkContent(page_content[i], content_class[i],i)
+    checkContent(page_content[i], content_class[i], i)
 }
 
-function checkContent(contentToCheck, classToCheck,contentDivNum) {
+function checkContent(contentToCheck, classToCheck, contentDivNum) {
     var page_wait = window.setInterval(function () {
         let current_content = woaFrame.contentWindow.document.getElementById(contentToCheck);
         if (current_content !== null) {
             window.clearInterval(page_wait);
             let current_content_class = current_content.getElementsByClassName(classToCheck)
             let contentUL = document.createElement('ul');
-            let contentLI = document.createElement('li');
-            contentLI.innerHTML = current_content_class[0].innerHTML
-            contentUL.appendChild(contentLI);
+
+            for (let i = 0; i < current_content_class.length; i++) {
+                let contentLI = document.createElement('li');
+                contentLI.innerHTML = current_content_class[i].innerHTML
+                contentUL.appendChild(contentLI);
+            }
             profileData[contentDivNum].appendChild(contentUL)
         }
     }, 50)
