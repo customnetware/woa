8ee4b7a195728e5d8e91c3d4066925cb9962d3d1
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-06 04:42:17 -0700

    Create new_resident_home.js

---
+++ new_resident_home.js
@@ -0,0 +1,11 @@
+var woaFrame = document.getElementById("MyFrame");
+const page_content = ["panel_news_content", "panel_messages_content", "panel_classifieds_content", "panel_resource_content", "panel_cal_content", "panel_discuss_content"];
+for (let i = 0; i < page_content.length; i++) {
+    var page_wait = window.setInterval(function () {
+        let current_content = woaFrame.contentWindow.document.getElementById(page_content[i]);
+        if (current_content !== null) {
+            window.clearInterval(page_wait);
+            alert(current_content + " found!")
+        }
+    }, 50)
+}
