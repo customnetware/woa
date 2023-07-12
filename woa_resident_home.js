0099b440edda88ecb5eca93bd98d1e57b4bbdeb6
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-12 10:49:58 -0700

    Update woa_resident_home.js

--- woa_resident_home.js
+++ woa_resident_home.js
@@ -1,20 +1,31 @@
-var woaFrame = document.getElementById("residentHome");
-var profileData = document.getElementById("profile_data").getElementsByClassName("card-body");
-var testWait = setInterval(function () {
-    let current_content = woaFrame.contentWindow.document.getElementById("panel_messages_content");
-    if (current_content !== null) {
-        let current_content_class = current_content.getElementsByClassName("message")
-        if (current_content_class !== null) {
-            if (current_content_class.length > 0) {
-                clearInterval(testWait)
-                let content_pp = document.createElement("p")
-                content_pp.setAttribute('style', 'padding: 0; margin-top: 0; px; margin-bottom: 3px;');
-                let contentURL = current_content_class[0].getElementsByTagName("a")[0].getAttribute("onclick");
-                let contentText = current_content_class[0].getElementsByTagName("a")[0].getAttribute("data-tooltip-title").split("by");
-                let contentBody = current_content_class[0].getElementsByTagName("a")[0].getAttribute("data-tooltip-text");
-            
-                profileData[1].innerHTML = "<b>" + contentText[0] + "</b><br />" + contentBody + "<a onclick=" + contentURL + " href='#'>&nbsp;<i>Read More</i></a>";
-            }
-        }
-    }
-}, 200);
+ woaFrame.onload = (function () {
+                displayContent("panel_news_content", "news", "data-tooltip-text", 0)
+                displayContent("panel_messages_content", "message", "data-tooltip-text", 1)
+                displayContent("panel_discuss_content", "post", "data-tooltip-text", 2)
+                displayContent("panel_classifieds_content", "classified", "data-tooltip-text", 3)
+                displayContent("panel_resource_content", "document", "data-item-viewurl", 5)
+                displayContent("panel_cal_content", "event", "href", 6)
+                waitScreen.style.display = "none";
+                if (profileTitle.getElementsByTagName("a").length > 0) {
+                    profileTitle.getElementsByTagName("a")[0].innerText = woaFrame.contentWindow.document.getElementsByClassName("clsHeader")[0].innerText
+                } else { profileTitle.innerText = woaFrame.contentWindow.document.getElementsByClassName("clsHeader")[0].innerText }
+
+                function displayContent(contentID, contentClass, contentAttr, contentPos) {
+                    let displayClass = woaFrame.contentWindow.document.getElementById(contentID).getElementsByClassName(contentClass)
+                    for (let i = 0; i < displayClass.length; i++) {
+                        let displayParagraph = document.createElement("p")
+                        displayParagraph.setAttribute("style", "padding: 0; margin-top: 0; px; margin-bottom: 3px;");
+                        let displayAttr = displayClass[i].getElementsByTagName("a")[0].getAttribute(contentAttr);
+                        let displayText = displayClass[i].getElementsByTagName("a")[0].innerText;
+                        if (contentPos == 1) {
+                            let displayURL = displayClass[i].getElementsByTagName("a")[0].getAttribute("onclick");
+                            let displayTitle = displayClass[i].getElementsByTagName("a")[0].getAttribute("data-tooltip-title").split("by");
+                            displayParagraph.innerHTML = "<b>" + displayTitle[0] + "</b><br />" + displayAttr + "<a onclick=" + displayURL + " href='#'>&nbsp;<i>Read More</i></a>";
+                        }
+                        if (contentPos == 2) { displayParagraph.innerHTML = displayClass[i].innerHTML + displayAttr; }
+                        if (contentPos == 0 || contentPos == 3) { displayParagraph.innerHTML = displayClass[i].innerHTML + "<br>" + displayAttr; }
+                        if (contentPos == 5 || contentPos == 6) { displayParagraph.innerHTML = "<a href=" + displayAttr + ">" + displayText + "</a>"; }
+                        profileData[contentPos].appendChild(displayParagraph)
+                    }
+                    }
+            })
