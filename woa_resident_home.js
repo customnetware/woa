bf598ef9d0e4a222c44f237e1d2b1e63b8ef41e5
customnetware <138068712+customnetware@users.noreply.github.com>
2023-07-12 17:48:18 -0700

    Update woa_resident_home.js

--- woa_resident_home.js
+++ woa_resident_home.js
@@ -1,32 +1,39 @@
- woaFrame.onload = (function () {
-    var woaFrame = document.getElementById("residentHome");
-                displayContent("panel_news_content", "news", "data-tooltip-text", 0)
-                displayContent("panel_messages_content", "message", "data-tooltip-text", 1)
-                displayContent("panel_discuss_content", "post", "data-tooltip-text", 2)
-                displayContent("panel_classifieds_content", "classified", "data-tooltip-text", 3)
-                displayContent("panel_resource_content", "document", "data-item-viewurl", 5)
-                displayContent("panel_cal_content", "event", "href", 6)
-                waitScreen.style.display = "none";
-                if (profileTitle.getElementsByTagName("a").length > 0) {
-                    profileTitle.getElementsByTagName("a")[0].innerText = woaFrame.contentWindow.document.getElementsByClassName("clsHeader")[0].innerText
-                } else { profileTitle.innerText = woaFrame.contentWindow.document.getElementsByClassName("clsHeader")[0].innerText }
-
-                function displayContent(contentID, contentClass, contentAttr, contentPos) {
-                    let displayClass = woaFrame.contentWindow.document.getElementById(contentID).getElementsByClassName(contentClass)
-                    for (let i = 0; i < displayClass.length; i++) {
-                        let displayParagraph = document.createElement("p")
-                        displayParagraph.setAttribute("style", "padding: 0; margin-top: 0; px; margin-bottom: 3px;");
-                        let displayAttr = displayClass[i].getElementsByTagName("a")[0].getAttribute(contentAttr);
-                        let displayText = displayClass[i].getElementsByTagName("a")[0].innerText;
-                        if (contentPos == 1) {
-                            let displayURL = displayClass[i].getElementsByTagName("a")[0].getAttribute("onclick");
-                            let displayTitle = displayClass[i].getElementsByTagName("a")[0].getAttribute("data-tooltip-title").split("by");
-                            displayParagraph.innerHTML = "<b>" + displayTitle[0] + "</b><br />" + displayAttr + "<a onclick=" + displayURL + " href='#'>&nbsp;<i>Read More</i></a>";
+            const woaFrame = document.getElementById("residentHome");
+            const contentDisplay = document.getElementById("profile_data").getElementsByClassName("card-body");
+            function getFrameContent(contentID, contentClass, ContentPos) {
+                let selectedContent = woaFrame.contentWindow.document.getElementById(contentID).getElementsByClassName(contentClass)
+                for (let p = 0; p < selectedContent.length; p++) {
+                    let selectedLink = selectedContent[p].getElementsByTagName("a")[0];
+                    let attr_href = selectedLink.getAttribute("href");
+                    let attr_onclick = selectedLink.getAttribute("onclick");
+                    let attr_text = selectedLink.getAttribute("data-tooltip-text");
+                    let attr_title = selectedLink.getAttribute("data-tooltip-title");
+                    let attr_viewurl = selectedLink.getAttribute("data-item-viewurl");
+                    let contentParagraph = document.createElement("p")
+                    contentParagraph.setAttribute('style', 'padding: 0; margin-top: 0; px; margin-bottom: 3px;');
+                    if (ContentPos == 0) { contentParagraph.innerHTML = "<b>" + attr_title + "</b><br />" + attr_text + "<a href=" + attr_href + ">&nbsp;<i class='fa fa-external-link'></i></a>"; }
+                    if (ContentPos == 1) { contentParagraph.innerHTML = "<b>" + attr_title.split("by")[0] + "</b><br />" + attr_text + "<a onclick=" + attr_onclick + " href='#'>&nbsp;<i class='fa fa-external-link'></i></a>"; }
+                    if (ContentPos == 2) { contentParagraph.innerHTML = "<b>" + attr_title + "</b><br />" + attr_text + "<a href=" + attr_href + ">&nbsp;<i class='fa fa-external-link'></i></a>"; }
+                    if (ContentPos == 3) { contentParagraph.innerHTML = "<b>" + attr_title + "</b><br />" + attr_text + "<a href=" + attr_href + ">&nbsp;<i class='fa fa-external-link'></i></a>"; }
+                    if (ContentPos == 5) {
+                        if (selectedLink.innerText.indexOf(".doc") >= 0) {
+                            contentParagraph.innerHTML = "<i class='fa fa-file-word-o'></i>&nbsp;<a href=" + attr_viewurl + ">" + selectedLink.innerText + "</a>";
+                        } else {
+                            contentParagraph.innerHTML = "<i class='fa fa-file-pdf-o'></i>&nbsp;<a href=" + attr_viewurl + ">" + selectedLink.innerText + "</a>";
                         }
-                        if (contentPos == 2) { displayParagraph.innerHTML = displayClass[i].innerHTML + displayAttr; }
-                        if (contentPos == 0 || contentPos == 3) { displayParagraph.innerHTML = displayClass[i].innerHTML + "<br>" + displayAttr; }
-                        if (contentPos == 5 || contentPos == 6) { displayParagraph.innerHTML = "<a href=" + displayAttr + ">" + displayText + "</a>"; }
-                        profileData[contentPos].appendChild(displayParagraph)
                     }
-                    }
-            })
+                    if (ContentPos == 6) { contentParagraph.innerHTML = "<a href=" + attr_href + ">" + selectedLink.innerText + "</a>"; }
+                    contentDisplay[ContentPos].appendChild(contentParagraph)
+                }
+            }
+            function getContents() {
+                getFrameContent("panel_news_content", "news", 0)
+                getFrameContent("panel_messages_content", "message", 1)
+                getFrameContent("panel_discuss_content", "post", 2)
+                getFrameContent("panel_classifieds_content", "classified", 3)
+                getFrameContent("panel_resource_content", "document", 5)
+                getFrameContent("panel_cal_content", "event", 6)
+            }
+            if (woaFrame.attachEvent) { woaFrame.attachEvent("onload", getContents); }
+            else if (woaFrame.addEventListener) { woaFrame.addEventListener("load", getContents); }
+            else { woaFrame.contentWindow.document.addEventListener("load", getContents); }
