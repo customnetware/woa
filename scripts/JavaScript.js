9f8f8f0254544e5e177ddfed16d125be61624ba2
Will Webb <willwebb@att.net>
2023-07-26 15:51:40 -0700

    test upload

--- scripts/JavaScript.js
+++ scripts/JavaScript.js
@@ -1,1 +1,11 @@
-﻿
+﻿const frameContent = [];
+frameContent[0] = "panel_news_content, news";
+frameContent[1] = "panel_messages_content, message";
+frameContent[2] = "panel_discuss_content, post";
+frameContent[3] = "panel_classifieds_content, classified";
+frameContent[4] = "panel_resource_content, document";
+frameContent[5] = "panel_cal_content, event";
+
+for (let i = 0; i < frameContent.length; i++) {
+
+}
