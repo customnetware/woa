4e9ff0fff99d0dac938604157bf7ecf77b6b3981
Will Webb <willwebb@att.net>
2023-07-30 01:49:04 -0700

    upload

--- scripts/woa_resident_home.js
+++ scripts/woa_resident_home.js
@@ -4,59 +4,59 @@ const bgImage = "this.style.backgroundImage='url(/images/icons/icon-message.png)
 const sentBy = "by Woodbridge HOA (Messenger@AssociationVoice.com)"
 const selGrps = ["8364", "11315"]
 function getFrameContent() {
-
-    const pagePanel = {
-        news: woaFrame.contentWindow.document.getElementById("panel_news_content").getElementsByClassName("news"),
-        message: woaFrame.contentWindow.document.getElementById("panel_messages_content").getElementsByClassName("message"),
-        post: woaFrame.contentWindow.document.getElementById("panel_discuss_content").getElementsByClassName("post"),
-        classified: woaFrame.contentWindow.document.getElementById("panel_classifieds_content").getElementsByClassName("classified"),
-        document: woaFrame.contentWindow.document.getElementById("panel_resource_content").getElementsByClassName("document"),
-        event: woaFrame.contentWindow.document.getElementById("panel_cal_content").getElementsByClassName("event")
-    }
-    for (const contentKey in pagePanel) {
-        const contentText = pagePanel[contentKey]
-        for (let p = 0; p < contentText.length; p++) {
-            let displayContent = document.getElementById(contentKey)
-            let displayLink = contentText[p].getElementsByTagName("a")[0]
-
-            let tRow = document.createElement("tr")
-            let tCell = document.createElement("td")
-            let tLink = document.createElement("a")
-
-            if (displayContent.id == "document" || displayContent.id == "event") {
-                tLink.href = displayLink.href
-                tLink.innerHTML = displayLink.innerHTML
-                tCell.appendChild(tLink)
-                tRow.appendChild(tCell)
-            } else {
-                if (displayContent.id !== "post" || (displayContent.id == "post" && selGrps.indexOf(displayLink.href.split("~")[1]) > -1)) {
-                    let topSpan = document.createElement("span")
-                    let btmSpan = document.createElement("span")
-                    topSpan.setAttribute("style", "font-weight: bold; display: block;")
-                    topSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-title").replace(sentBy, "")))
-                    btmSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-text")))
-
-                    tLink.href = displayLink.href
-                    tLink.className = "fa fa-external-link formatLink"
-
-                    tCell.appendChild(topSpan)
-                    tCell.appendChild(btmSpan)
-                    tCell.appendChild(tLink)
-
-                    tRow.appendChild(tCell)
-                    displayContent.appendChild(tRow)
-                }
-            }
-            displayContent.appendChild(tRow)
+    try {
+        const pagePanel = {
+            news: woaFrame.contentWindow.document.getElementById("panel_news_content").getElementsByClassName("news"),
+            message: woaFrame.contentWindow.document.getElementById("panel_messages_content").getElementsByClassName("message"),
+            post: woaFrame.contentWindow.document.getElementById("panel_discuss_content").getElementsByClassName("post"),
+            classified: woaFrame.contentWindow.document.getElementById("panel_classifieds_content").getElementsByClassName("classified"),
+            document: woaFrame.contentWindow.document.getElementById("panel_resource_content").getElementsByClassName("document"),
+            event: woaFrame.contentWindow.document.getElementById("panel_cal_content").getElementsByClassName("event")
         }
+        for (const contentKey in pagePanel) {
+            const contentText = pagePanel[contentKey]
+            for (let p = 0; p < contentText.length; p++) {
+                let displayContent = document.getElementById(contentKey)
+                let displayLink = contentText[p].getElementsByTagName("a")[0]
+
+                let tRow = document.createElement("tr")
+                let tCell = document.createElement("td")
+                let tLink = document.createElement("a")
+
+                if (displayContent.id == "document" || displayContent.id == "event") {
+                    tLink.href = displayLink.href
+                    tLink.innerHTML = displayLink.innerHTML
+                    tCell.appendChild(tLink)
+                    tRow.appendChild(tCell)
+                } else {
+                    if (displayContent.id !== "post" || (displayContent.id == "post" && selGrps.indexOf(displayLink.href.split("~")[1]) > -1)) {
+                        let topSpan = document.createElement("span")
+                        let btmSpan = document.createElement("span")
+                        topSpan.setAttribute("style", "font-weight: bold; display: block;")
+                        topSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-title").replace(sentBy, "")))
+                        btmSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-text")))
+
+                        tLink.href = displayLink.href
+                        tLink.className = "fa fa-external-link formatLink"
+
+                        tCell.appendChild(topSpan)
+                        tCell.appendChild(btmSpan)
+                        tCell.appendChild(tLink)
+
+                        tRow.appendChild(tCell)
+                        displayContent.appendChild(tRow)
+                    }
+                }
+                displayContent.appendChild(tRow)
+            }
+        }
+        document.getElementById("overlay").style.display = "none"
+        getProfileInfo()
     }
-    document.getElementById("overlay").style.display = "none"
-    getProfileInfo()
-    //try { }
-    //catch (err) {
-    //    document.getElementById("overlay").style.display = "none"
-    //    document.getElementById("errText").innerHTML = err.message
-    //}
+    catch (err) {
+        document.getElementById("overlay").style.display = "none"
+        document.getElementById("errText").innerHTML = err.message
+    }
 }
 function getProfileInfo() {
 
