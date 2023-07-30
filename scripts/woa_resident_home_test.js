faf3b48780f42f47f2ab4b5b2614cbc53629dc0f
Will Webb <willwebb@att.net>
2023-07-29 23:45:55 -0700

    update

--- scripts/woa_resident_home_test.js
+++ scripts/woa_resident_home_test.js
@@ -4,64 +4,56 @@ const woaFrame = document.getElementById("residentHome");
 const bgImage = "this.style.backgroundImage='url(/images/icons/icon-message.png)';"
 const sentBy = "by Woodbridge HOA (Messenger@AssociationVoice.com)"
 const selGrps = ["8364", "11315"]
-const frameContent = [];
-frameContent[0] = "panel_news_content,news";
-frameContent[1] = "panel_messages_content,message";
-frameContent[2] = "panel_discuss_content,post";
-frameContent[3] = "panel_classifieds_content,classified";
-frameContent[4] = "panel_resource_content,document";
-frameContent[5] = "panel_cal_content,event";
-
 function getFrameContent() {
-    for (let i = 0; i < frameContent.length; i++) {
-        let contentID = frameContent[i].split(",")
-        let selectedContent = woaFrame.contentWindow.document.getElementById(contentID[0]).getElementsByClassName(contentID[1]);
-
-        for (let p = 0; p < selectedContent.length; p++) {
-            let displayContent = document.getElementById(contentID[1])
-            let displayLink = selectedContent[p].getElementsByTagName("a")[0]
+    let woaDocument = woaFrame.contentWindow.document
+    const pagePanel = {
+        news: woaDocument.getElementById("panel_news_content").getElementsByClassName("news"),
+        message: woaDocument.getElementById("panel_messages_content").getElementsByClassName("message"),
+        post: woaDocument.getElementById("panel_discuss_content").getElementsByClassName("post"),
+        classified: woaDocument.getElementById("panel_classifieds_content").getElementsByClassName("classified"),
+        document: woaDocument.getElementById("panel_resource_content").getElementsByClassName("document"),
+        event: woaDocument.getElementById("panel_cal_content").getElementsByClassName("event")
+    }
+    for (const contentKey in pagePanel) {
+        const contentText = pagePanel[contentKey]
+        for (let p = 0; p < contentText.length; p++) {
+            let displayContent = document.getElementById(contentKey)
+            let displayLink = contentText[p].getElementsByTagName("a")[0]
 
             let tRow = document.createElement("tr")
             let tCell = document.createElement("td")
             let tLink = document.createElement("a")
 
-            if (contentID[1] == "document" || contentID[1] == "event") {
+            if (displayContent.id == "document" || displayContent.id == "event") {
                 tLink.href = displayLink.href
                 tLink.innerHTML = displayLink.innerHTML
                 tCell.appendChild(tLink)
                 tRow.appendChild(tCell)
-                displayContent.appendChild(tRow)
             } else {
-                if (contentID[1] !== "post" || (contentID[1] == "post" && selGrps.indexOf(displayLink.href.split("~")[1]) > -1)) {
-                    let topSpan = document.createElement("span");
-                    let btmSpan = document.createElement("span");
-                    topSpan.setAttribute("style", "font-weight: bold; display: block;");
-                    topSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-title").replace(sentBy, "")));
-                    btmSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-text")));
+                if (displayContent.id !== "post" || (displayContent.id == "post" && selGrps.indexOf(displayLink.href.split("~")[1]) > -1)) {
+                    let topSpan = document.createElement("span")
+                    let btmSpan = document.createElement("span")
+                    topSpan.setAttribute("style", "font-weight: bold; display: block;")
+                    topSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-title").replace(sentBy, "")))
+                    btmSpan.appendChild(document.createTextNode(displayLink.getAttribute("data-tooltip-text")))
 
-                    tLink.href = displayLink.href;
+                    tLink.href = displayLink.href
                     tLink.className = "fa fa-external-link formatLink"
 
-                    tCell.appendChild(topSpan);
-                    tCell.appendChild(btmSpan);
+                    tCell.appendChild(topSpan)
+                    tCell.appendChild(btmSpan)
                     tCell.appendChild(tLink)
 
                     tRow.appendChild(tCell)
                     displayContent.appendChild(tRow)
                 }
             }
+            displayContent.appendChild(tRow)
         }
     }
-    document.getElementById("overlay").style.display = "none";
-    getProfileInfo()
-
-    //try { }
-    //catch (err) {
-    //    document.getElementById("overlay").style.display = "none";
-    //    document.getElementById("errText").innerHTML = err.message;
-    //}
+    document.getElementById("overlay").style.display = "none"
+    getProfileInfo();
 }
-
 function getProfileInfo() {
 
     findImage = setInterval(function () {
@@ -71,9 +63,6 @@ function getProfileInfo() {
             if (new Date().getTime() - startTime > 15000) { clearInterval(findImage); }
         }
     }, 25);
-
-
-
 
     if (document.getElementById("resDisplayName") !== null) {
         document.getElementById("resDisplayName").innerText = "My Woodbridge";
@@ -101,6 +90,11 @@ else if (window.addEventListener) { window.addEventListener("load", getFrameCont
 else { window.contentWindow.document.addEventListener("load", getFrameContent); }
 
 
+    //try { }
+    //catch (err) {
+    //    document.getElementById("overlay").style.display = "none";
+    //    document.getElementById("errText").innerHTML = err.message;
+    //}
 
 
 
