8ccedb863fb5c27c5000b77af2dd1e1dde1537db
Will Webb <willwebb@att.net>
2023-07-29 23:43:58 -0700

    update

--- scripts/JavaScript.js
+++
@@ -1,7 +0,0 @@
-﻿const selGrps = "GeneralTest_Group"
-const contentID = "post"
-let contentCheck = function () {
-    if (contentID == "post") {
-        return woaFrame.contentWindow.document.getElementById(contentID[0]).getElementsByClassName("discussion")[0].getElementsByTagName("a")[0].innerHTML
-    } else { return null }
-}
