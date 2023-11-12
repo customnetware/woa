8506a5bb65e68eee094d928b2dd012d4dc958ea1
Will Webb <willwebb@att.net>
2023-11-11 19:33:05 -0800

    sort update

--- scripts/folders_new.js
+++ scripts/folders_new.js
@@ -23,15 +23,13 @@ function showDocuments(selectedFolder, previousFolder, PreviousFolderName) {
                     let docIcon1 = document.createElement("i")
 
                     docIcon1.className = "fa fa-folder-open-o formatIcon"
-                    docLink.href = "javascript:showDocuments('" + previousFolder+"')"
+                    docLink.href = "javascript:showDocuments('" + previousFolder + "')"
 
                     docLink.innerHTML = PreviousFolderName
                     docRow.appendChild(docIcon1)
                     docRow.appendChild(docLink)
                     document.getElementById("document").appendChild(docRow)
                 }
-
-
                 for (let d = 0; d < documentList.length; d++) {
                     let docRow = document.createElement("span")
                     let docLink = document.createElement("a")
@@ -41,7 +39,7 @@ function showDocuments(selectedFolder, previousFolder, PreviousFolderName) {
                     let localDocID = remoteDoc.id.replace("f", "").replace("d", "")
                     if (isFolder == true) {
                         docIcon.className = "fa fa-folder-o formatIcon"
-                        docLink.href = "javascript:showDocuments('" + localDocID + "','" + selectedFolder + "','"+remoteDoc.innerText+"');"
+                        docLink.href = "javascript:showDocuments('" + localDocID + "','" + selectedFolder + "','" + remoteDoc.innerText + "');"
                     } else {
                         docIcon.className = "fa fa-file-pdf-o formatIcon"
                         docLink.href = documents.getElementById("contentsDoc" + localDocID).getElementsByTagName("a")[2].href
@@ -59,7 +57,25 @@ function showDocuments(selectedFolder, previousFolder, PreviousFolderName) {
             })
     }
 }
+function screenSort(sortDirection) {
+    let screens = document.getElementById("document").getElementsByTagName("span")
 
+
+    let sortScreens = []
+    for (let s = 1; s < screens.length; s++) {
+        sortScreens.push(screens[s].innerHTML)
+    }
+
+    sortScreens.sort()
+    if (sortDirection == "U") {  sortScreens.reverse() }
+  
+
+    for (let s = 0; s < sortScreens.length; s++) {
+
+        screens[s + 1].innerHTML = sortScreens[s]
+    }
+
+}
 $(window).load(function () {
     showDocuments('000000', '000000')
 })
