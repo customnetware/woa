81c9f66146471881e65aeac26316d6dd3c79ebb2
Will Webb <willwebb@att.net>
2023-12-05 22:12:59 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -1,86 +1,78 @@
 let currentDate = new Date()
-$.get("/homepage/28118/resident-home-page", function () { })
-    .done(function (responseText) {
-        let myWoodbridge = new DOMParser().parseFromString(responseText, "text/html")
-        let recentItems = myWoodbridge.getElementsByClassName("message")
-        for (let p = 0; p < recentItems.length; p++) {
-            let itemContent = recentItems[p].getElementsByTagName("a")[0]
-            addToNotifications(itemContent.getAttribute("data-tooltip-title").split("by")[0], itemContent.getAttribute("data-tooltip-text"), itemContent.href, "fa fa-envelope-o")
-        }
-        showPhotos(myWoodbridge)
-        document.getElementsByClassName("clsHeader")[0].innerHTML = myWoodbridge.getElementsByClassName("clsHeader")[0].innerHTML
-    })
-$.get("/news/list/28118/news-announcements", function () { })
-    .done(function (responseText) {
-        let newsArticles = new DOMParser().parseFromString(responseText, "text/html")
-        let articleTitle = newsArticles.getElementsByClassName("clsHeader")
-        let articleContent = newsArticles.getElementsByClassName("clsBodyText")
-
-        for (let p = 0; p < articleContent.length; p++) {
-            let articleLink = articleTitle[p].parentElement.getElementsByTagName("div")[2].getElementsByTagName("a")[0].href
-            addToNotifications(articleTitle[p].innerText, articleContent[p].innerText, articleLink, "fa fa-newspaper-o")
-        }
-    })
-$.get("/Discussion/28118~8364", function () { })
-    .done(function (responseText) {
-        let forum = new DOMParser().parseFromString(responseText, "text/html")
-        let postHeaders = forum.querySelectorAll("[id^=msgHeader]")
-        let postContents = forum.querySelectorAll("[id^=contents]")
-        for (let h = 0; h < postHeaders.length; h++) {
-            let messageTexts = postContents[h].getElementsByClassName("clsBodyText")
-            let messageAuthor = postContents[h].getElementsByClassName("respAuthorWrapper")
-            let messageContacts = postContents[h].getElementsByClassName("respReplyWrapper")
-            let postDate = new Date(messageAuthor[messageAuthor.length - 1].innerText.split("-")[1])
-            let dayDiff = (currentDate - postDate) / (1000 * 3600 * 24)
-            if (dayDiff <= 90) {
-                addToNotifications(postHeaders[h].innerText, messageTexts[0].innerText + messageAuthor[0].innerText, messageContacts[0].getElementsByTagName("a")[0].href, "fa fa-comments-o fa-lg")
+function getResidentHomePage() {
+    let emailList = document.getElementById("recentEmails").getElementsByClassName("card-body")[0]
+    emailList.innerHTML = ""
+    $.get("/homepage/28118/resident-home-page.html", function () { })
+        .done(function (responseText) {
+            let myWoodbridge = new DOMParser().parseFromString(responseText, "text/html")
+            let recentItems = myWoodbridge.getElementsByClassName("message")
+            document.getElementsByClassName("clsHeader")[0].innerHTML = myWoodbridge.getElementsByClassName("clsHeader")[0].innerHTML
+            showPhotos(myWoodbridge)
+            for (let p = 0; p < recentItems.length; p++) {
+                let itemContent = recentItems[p].getElementsByTagName("a")[0]
+                let currentItem = document.createElement("p")
+                let itemTitle = document.createElement("span")
+                let itemLink = document.createElement("a")
+                itemTitle.appendChild(document.createTextNode(itemContent.getAttribute("data-tooltip-title").split("by")[0]))
+                itemLink.href = itemContent.href
+                itemLink.className = "fa fa-arrow-right fa-lg formatLink"
+                currentItem.appendChild(itemTitle)
+                currentItem.appendChild(document.createTextNode(itemContent.getAttribute("data-tooltip-text")))
+                currentItem.appendChild(itemLink)
+                emailList.appendChild(currentItem)
             }
-        }
-    })
-$.get("/resourcecenter/28118/resource-center", function () { })
-    .done(function (responseText) {
-        let documents = new DOMParser().parseFromString(responseText, "text/html")
-        let documentName = documents.getElementById("contents540434").querySelectorAll("[id^=d]")
-        let documentLink = documents.getElementById("contents540434").querySelectorAll('a[title="View On-line"]')
-        let newsLetterName = documents.getElementById("contents951754").querySelectorAll("[id^=d]")
-        let newsLettertLink = documents.getElementById("contents951754").querySelectorAll('a[title="View On-line"]')
+        })
+}
+function getNewsAndAnnouncements() {
+    let newsList = document.getElementById("recentNews").getElementsByClassName("card-body")[0]
+    newsList.innerHTML = ""
+    $.get("/news/list/28118/news-announcements.html", function () { })
+        .done(function (responseText) {
+            let newsArticles = new DOMParser().parseFromString(responseText, "text/html")
+            let articleTitle = newsArticles.getElementsByClassName("clsHeader")
+            let articleContent = newsArticles.getElementsByClassName("clsBodyText")
+            for (let p = 0; p < articleContent.length; p++) {
+                let currentItem = document.createElement("p")
+                let itemTitle = document.createElement("span")
+                let itemLink = document.createElement("a")
+                itemLink.href = articleTitle[p].parentElement.getElementsByTagName("div")[2].getElementsByTagName("a")[0].href
+                itemLink.className = "fa fa-arrow-right fa-lg formatLink"
+                itemTitle.appendChild(document.createTextNode(articleTitle[p].innerText))
+                currentItem.appendChild(itemTitle)
+                currentItem.appendChild(document.createTextNode(articleContent[p].innerText))
+                currentItem.appendChild(itemLink)
 
-        let newsLetterItem = document.createElement("span")
-        let selectedDoc = document.createElement("a")
-        selectedDoc.innerHTML = newsLetterName[newsLetterName.length - 1].innerHTML
-        selectedDoc.href = newsLettertLink[newsLetterName.length - 1].href
-        newsLetterItem.appendChild(selectedDoc)
-        document.getElementById("recentNewsletters").appendChild(newsLetterItem)
-
-        for (let p = 0; p < documentName.length; p++) {
-            let resourceItem = document.createElement("span")
+                newsList.appendChild(currentItem)
+            }
+        })
+}
+function getResourceCenter() {
+    let docList = document.getElementById("recentFlyers").getElementsByClassName("card-body")[0]
+    $.get("/resourcecenter/28118/resource-center.html", function () { })
+        .done(function (responseText) {
+            let documents = new DOMParser().parseFromString(responseText, "text/html")
+            let documentName = documents.getElementById("contents540434").querySelectorAll("[id^=d]")
+            let documentLink = documents.getElementById("contents540434").querySelectorAll('a[title="View On-line"]')
+            let newsLetterName = documents.getElementById("contents951754").querySelectorAll("[id^=d]")
+            let newsLettertLink = documents.getElementById("contents951754").querySelectorAll('a[title="View On-line"]')
+            let newsLetterItem = document.createElement("span")
             let selectedDoc = document.createElement("a")
-            selectedDoc.innerHTML = documentName[p].innerHTML
-            selectedDoc.href = documentLink[p].href
-            resourceItem.appendChild(selectedDoc)
-            document.getElementById("document").appendChild(resourceItem)
-            if (document.getElementById("document").getElementsByTagName("span").length == 15) { break }
-        }
-        document.getElementById("documentIcon").className = "fa fa-file-text-o"
-    })
-$.get("/classified/search/28118~480182/classifieds", function () { })
-    .done(function (responseText) {
-        let classifieds = new DOMParser().parseFromString(responseText, "text/html")
-        let classifiedTitle = classifieds.querySelectorAll('.clsBodyText:not(.hidden-md-up,.hidden-sm-down)')
-        let classifiedSummary = classifieds.getElementsByClassName("clsBodyText hidden-md-up")
-        let classifiedBody = classifieds.getElementsByClassName("clsBodyText hidden-sm-down")
-        for (let p = 0; p < classifiedTitle.length; p++) {
-            addToNotifications(classifiedTitle[p].getElementsByTagName("a")[0].innerHTML, classifiedBody[p].childNodes[0].nodeValue, "#", "fa fa-shopping-cart")
-        }
-    })
-let profileID = document.getElementById("HeaderPublishAuthProfile").href.split("(")[1].split(",")[0]
-let profilePage = (window.location.hostname == "localhost") ? "/Member/28118~" + profileID + ".html" : "/Member/28118~" + profileID
-$.get(profilePage, function () {
-}).done(function (responseText) {
-    let profileDoc = new DOMParser().parseFromString(responseText, "text/html")
-    document.getElementById("profileImage").src = profileDoc.getElementsByTagName("img")[0].src
-})
-
+            selectedDoc.innerHTML = newsLetterName[newsLetterName.length - 1].innerHTML
+            selectedDoc.href = newsLettertLink[newsLetterName.length - 1].href
+            newsLetterItem.appendChild(selectedDoc)
+            docList.appendChild(newsLetterItem)
+            for (let p = 0; p < documentName.length; p++) {
+                let resourceItem = document.createElement("span")
+                let selectedDoc = document.createElement("a")
+                selectedDoc.innerHTML = documentName[p].innerHTML
+                selectedDoc.href = documentLink[p].href
+                resourceItem.appendChild(selectedDoc)
+                docList.appendChild(resourceItem)
+             /*   if (document.getElementById("document").getElementsByTagName("span").length == 15) { break }*/
+            }
+        
+        })
+}
 function showPhotos(galleryPage) {
     try {
         let newPicList = document.getElementById("recentPhotos").getElementsByTagName("div")
@@ -95,41 +87,139 @@ function showPhotos(galleryPage) {
     } catch (error) { }
 
 }
-function addToNotifications(title, content, link, iconType) {
-    let itemType = document.createElement("i")
-    let itemTitle = document.createElement("span")
-    let itemLink = document.createElement("a")
-    let recentItem = document.createElement("p")
+function getProfilePage() {
+    let profileID = document.getElementById("HeaderPublishAuthProfile").href.split("(")[1].split(",")[0]
+    let profilePage = (window.location.hostname == "localhost") ? "/Member/28118~" + profileID + ".html" : "/Member/28118~" + profileID
+    $.get(profilePage, function () {
+    }).done(function (responseText) {
+        let profileDoc = new DOMParser().parseFromString(responseText, "text/html")
+        document.getElementById("profileImage").src = profileDoc.getElementsByTagName("img")[0].src
+    })
+}
+function getClassifiedAds() {
+    let classifiedsList = document.getElementById("recentAds").getElementsByClassName("card-body")[0]
+    classifiedsList.innerHTML = ""
+    $.get("/classified/search/28118~480182/classifieds.html", function () { })
+        .done(function (responseText) {
+            let classifieds = new DOMParser().parseFromString(responseText, "text/html")
+            let classifiedTitle = classifieds.querySelectorAll('.clsBodyText:not(.hidden-md-up,.hidden-sm-down)')
+            let classifiedSummary = classifieds.getElementsByClassName("clsBodyText hidden-md-up")
+            let classifiedBody = classifieds.getElementsByClassName("clsBodyText hidden-sm-down")
+            for (let p = 0; p < classifiedTitle.length; p++) {
+                let currentItem = document.createElement("p")
+                let itemTitle = document.createElement("span")
+                let itemLink = document.createElement("a")
+                itemTitle.appendChild(document.createTextNode(classifiedTitle[p].getElementsByTagName("a")[0].innerHTML))
+                currentItem.appendChild(itemTitle)
+                currentItem.appendChild(document.createTextNode(classifiedBody[p].childNodes[0].nodeValue))
+                classifiedsList.appendChild(currentItem)
+            }
+        })
+}
+function getDiscussionGroups(NumOfDays) {
+    let selectedGroups = [8030, 8364]
+    let postList = document.getElementById("recentPosts").getElementsByClassName("card-body")[0]
+    postList.innerHTML = ""
+    for (let h = 0; h < selectedGroups.length; h++) {
+        let selectedPost = (window.location.hostname == "localhost") ? "/Discussion/28118~" + selectedGroups[h] + ".html" : "/Discussion/28118~" + selectedGroups[h]
+        $.get(selectedPost, function () { })
+            .done(function (responseText) {
+                let forum = new DOMParser().parseFromString(responseText, "text/html")
+                let postHeaders = forum.querySelectorAll("[id^=msgHeader]")
+                let postContents = forum.querySelectorAll("[id^=contents]")
+                for (let h = 0; h < postHeaders.length; h++) {
+                    let messageTexts = postContents[h].getElementsByClassName("clsBodyText")
+                    let messageAuthor = postContents[h].getElementsByClassName("respAuthorWrapper")
+                    let messageContacts = postContents[h].getElementsByClassName("respReplyWrapper")
+                    let postDate = new Date(messageAuthor[messageAuthor.length - 1].innerText.split("-")[1])
+                    let dayDiff = (currentDate - postDate) / (1000 * 3600 * 24)
+                    if (dayDiff <= NumOfDays) {
+                        let currentPost = document.createElement("p")
+                        let postHeader = document.createElement("span")
+                        let postMessage = document.createElement("span")
+                        let postAuthor = document.createElement("span")
+                        let postReply = document.createElement("a")
 
-    itemType.className = iconType
-    itemType.style.paddingRight = "7px"
+                        postHeader.appendChild(document.createTextNode(postHeaders[h].innerText))
+                        postReply.className = "fa fa-reply fa-lg formatLink"
+                        postReply.href = messageContacts[0].getElementsByTagName("a")[0].href
+                        postHeader.appendChild(postReply)
 
-    itemTitle.appendChild(itemType)
-    itemTitle.appendChild(document.createTextNode(title))
+                        if (messageTexts.length > 1) {
+                            let replys = document.createElement("a")
+                            replys.className = "fa fa-comments fa-lg formatLink"
 
-    if (iconType.includes("comments")) {
-        let replyLink = document.createElement("a")
-        replyLink.href = link
-        replyLink.innerHTML = "Reply"
-        itemTitle.appendChild(replyLink)
-    }
-
-    recentItem.appendChild(itemTitle)
-    recentItem.appendChild(document.createTextNode(content))
-
-    itemLink.href = link
-    itemLink.className = "fa fa-arrow-right fa-lg formatLink"
-    if (iconType.includes("envelope") || iconType.includes("newspaper")) { recentItem.appendChild(itemLink) }
-
-
-    if (iconType.includes("envelope")) {
-        document.getElementById("message").insertBefore(recentItem, document.getElementById("message").children[0])
-    } else {
-        document.getElementById("message").appendChild(recentItem)
+                            replys.href = "javascript:showReplies(" + document.getElementsByClassName("groupPost").length + ")"
+                            postHeader.appendChild(replys)
+                            postHeader.appendChild(document.createTextNode(" (" + (messageTexts.length - 1) + ") "))
+                        }
+                        postMessage.appendChild(document.createTextNode(messageTexts[0].innerText))
+                        postAuthor.appendChild(document.createTextNode(messageAuthor[0].innerText))
+                        currentPost.appendChild(postHeader)
+                        currentPost.appendChild(postMessage)
+                        currentPost.appendChild(postAuthor)
+                        for (let p = 1; p < messageTexts.length; p++) {
+                            let replyMessage = document.createElement("span")
+                            let replyAuthor = document.createElement("span")
+                            replyMessage.appendChild(document.createTextNode(messageTexts[p].innerText))
+                            replyAuthor.appendChild(document.createTextNode(messageAuthor[p].innerText))
+                            currentPost.appendChild(replyMessage)
+                            currentPost.appendChild(replyAuthor)
+                        }
+                        currentPost.className = "groupPost"
+                        postList.appendChild(currentPost)
+                    }
+                }
+            })
     }
 }
+function showReplies(p_id) {
+    let posts = document.getElementsByClassName("groupPost")
+    for (let p = 0; p < posts.length; p++) {
+        let replies = posts[p].getElementsByTagName("span")
+        if (replies.length > 3) {
+            for (let r = 3; r < replies.length; r++) {
+                if (p == p_id) { if (replies[r].style.display == "block") { replies[r].style.display = "none" } else { replies[r].style.display = "block" } } else { replies[r].style.display = "none" }
+            }
+        }
+    }
+}
+function notifySettings(settingID) {
+    let mButtons = document.getElementById("notificationsSettings").getElementsByTagName("button")
+    let recentItems = ["recentEmails", "recentPosts", "recentNews", "recentAds"]
+    for (let p = 0; p < mButtons.length; p++) { if (p == settingID) { mButtons[settingID].style.backgroundColor = "#0275d8" } else { mButtons[p].style.backgroundColor = "#F0F0F0" } }
+    for (let h = 0; h < recentItems.length; h++) {
+        if (h == settingID) {
+            document.getElementById(recentItems[h]).className = "collapse show"
+            document.getElementById(recentItems[h]).parentElement.getElementsByTagName("div")[0].className = "card-header"
+            document.getElementById(recentItems[h]).parentElement.getElementsByTagName("div")[0].setAttribute("aria-expanded", "true")
+        } else {
+            document.getElementById(recentItems[h]).className = "collapse"
+            document.getElementById(recentItems[h]).parentElement.getElementsByTagName("div")[0].className = "card-header collapsed"
+            document.getElementById(recentItems[h]).parentElement.getElementsByTagName("div")[0].setAttribute("aria-expanded", "false")
+        }
 
+    }
+    if (settingID == 0) { getResidentHomePage() }
+    if (settingID == 1) { getDiscussionGroups(365) }
+    if (settingID == 2) { getClassifiedAds() }
+    if (settingID == 3) { getNewsAndAnnouncements() }
+    if (settingID == 4) { location.reload() }
+}
+function showHistory() {
+    let selectedID = document.getElementsByClassName("col-md-6")[0].getElementsByClassName("collapse show")[0].id
+    if (selectedID == "recentEmails") { getResidentHomePage() }
+    if (selectedID == "recentPosts") { getDiscussionGroups(365) }
+    if (selectedID == "recentNews") { getClassifiedAds() }
+    if (selectedID == "recentAds") { getNewsAndAnnouncements() }
+}
 $(window).load(function () {
+    getProfilePage()
+    getResourceCenter()
+    getNewsAndAnnouncements()
+    getDiscussionGroups(90)
+    getClassifiedAds()
+    getResidentHomePage()
     document.getElementById("loadIcon").style.display = "none"
     document.getElementById("pageRow").style.visibility = "visible"
 
