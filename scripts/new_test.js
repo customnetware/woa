78fb94c9dccb6faa395bcfb6cecea97b347d2a54
Will Webb <willwebb@att.net>
2023-12-10 16:07:39 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -251,9 +251,7 @@ function getEmail(messageID) {
             let emailHeader = selectedEmail.getElementById("tblMsgHeader")
             let emailBody = selectedEmail.getElementsByTagName("table")[1]
             let emailSubHeader = emailHeader.getElementsByClassName("clsGridDetail")
-
-            document.getElementById("recentEmails").innerHTML = emailSubHeader[3].innerHTML
-
+            document.getElementById("recentEmails").getElementsByTagName("span")[2] = emailSubHeader[3].innerHTML
             emailDisplay.innerHTML = ""
 
             var pTags = emailBody.getElementsByTagName('p')
@@ -266,12 +264,8 @@ function getEmail(messageID) {
             emailDisplay.innerHTML = emailBody.innerHTML
 
             //let emailDisplay = document.getElementById("showEmailAlert").getElementsByClassName("modal-body")[0]
-
             //let emailSubject = document.getElementById("showEmailAlertLabel")
             //emailSubject.innerHTML = emailSubHeader[3].innerHTML
-
-
-
             ////emailDisplay.innerHTML = emailBody.innerHTML
             ////$('#showEmailAlert').modal('show')
         })
