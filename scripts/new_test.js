fbd1b80e2332942b901ad9d5ad9e982df6aeb545
Will Webb <willwebb@att.net>
2023-12-10 16:10:19 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -251,7 +251,7 @@ function getEmail(messageID) {
             let emailHeader = selectedEmail.getElementById("tblMsgHeader")
             let emailBody = selectedEmail.getElementsByTagName("table")[1]
             let emailSubHeader = emailHeader.getElementsByClassName("clsGridDetail")
-            document.getElementById("recentEmails").getElementsByTagName("span")[2] = emailSubHeader[3].innerHTML
+            document.getElementById("recentEmails").getElementsByTagName("span")[1] = emailSubHeader[3].innerHTML
 
             emailDisplay.innerHTML = ""
 
