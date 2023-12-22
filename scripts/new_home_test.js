673e714f001547108f2d3997b09406bf896a1c40
Will Webb <willwebb@att.net>
2023-12-21 18:32:24 -0800

    update

--- scripts/new_home_test.js
+++ scripts/new_home_test.js
@@ -1,6 +1,7 @@
 const currentDate = new Date()
-function pageLocation(URLString) { return (window.location.hostname == "localhost") ? URLString + ".html" : URLString }
-
+function pageLocation(URLString) {
+    return (window.location.hostname == "localhost") ? URLString + ".html" : URLString
+}
 function getDiscussionGroupPosts() {
     let selectedGroup = "8364"
     $("#recentPosts").load(pageLocation("/Discussion/28118~" + selectedGroup) + " .ThreadContainer", function () {
@@ -20,20 +21,26 @@ function getSelectedPost(postIndex) {
     let indentToRemove = currentPosts[postIndex].getElementsByClassName("respDiscChildPost")
     let replyLinks = currentPosts[postIndex].getElementsByClassName("respReplyWrapper")
     let replyName = currentPosts[postIndex].getElementsByClassName("respAuthorWrapper")
-for (i = 1; i < replyLinks.length; i++) {
-    replyLinks[i].insertBefore(document.createTextNode(replyName[i].innerHTML), replyLinks[i].firstChild);
-    replyName[i].style.display = "none"
-}
 
-    for (c = 0; c < indentToRemove.length; c++) {
-       indentToRemove[c].classList = "respDiscChildPost"
-        indentToRemove[c].removeAttribute("style")
-    }
 
-    let replyFontSize = currentPost.getElementsByTagName("span")
-    let replyBorder = currentPosts[postIndex].getElementsByClassName("respDiscWrapper")
+
+
+
     for (c = 0; c < currentPosts.length; c++) { if (c !== postIndex) { currentPosts[c].getElementsByClassName("row")[1].style.display = "none" } }
     if (currentPost.style.display == "none") {
+
+    for (i = 0; i < replyLinks.length; i++) {
+        if (replyName[i].style.display !== "none") {
+            replyLinks[i].insertBefore(document.createTextNode(replyName[i].innerText.trim()), replyLinks[i].firstChild)
+            replyName[i].style.display = "none"
+        }
+    }
+    for (c = 0; c < indentToRemove.length; c++) {
+        indentToRemove[c].classList = "respDiscChildPost"
+        indentToRemove[c].removeAttribute("style")
+    }
+    let replyFontSize = currentPost.getElementsByTagName("span")
+    let replyBorder = currentPosts[postIndex].getElementsByClassName("respDiscWrapper")
 
         currentPost.style.display = ""
         currentPost.style.border = ""
@@ -52,46 +59,11 @@ for (i = 1; i < replyLinks.length; i++) {
             divTag.innerText = selectedParagraph.innerText.replace('"', '').trim()
             selectedParagraph.parentNode.replaceChild(divTag, selectedParagraph)
         }
-
     } else { currentPost.style.display = "none" }
 }
-
-let linksToHide = currentPosts[postIndex].getElementsByClassName("respReplyWrapper")respDiscTopic
-let authorToShow = currentPosts[postIndex].getElementsByClassName("respAuthorWrapper")
-let testCSS = currentPosts[postIndex].getElementsByClassName("respDiscChildPost")
-
-
-
-for (c = 0; c < testCSS.length; c++) {
-    testCSS[c].style.paddingTop = "10px"
-    let testSpan = document.createElement("div")
-    testSpan.style.fontWeight = "700"
-    testSpan.innerHTML = authorToShow[c + 1].textContent
-    testSpan.appendChild(document.createElement("hr"))
-    testCSS[c].appendChild(testSpan)
-}
-
-for (i = 1; i < linksToHide.length; i++) {
-    linksToHide[i].textContent = authorToShow[i].textContent
-
-    linksToHide[i].style.paddingLeft = "15px"
-    authorToShow[i].style.display = "none"
-    linksToHide[i].style.display = "none"
-}
-
-
-
-for (i = linksToHide.length - 1; i >= 1; i--) {
-    linksToHide[i].innerHTML = authorToShow[i].innerHTML
-    linksToHide[i].style.paddingLeft = "15px"
-    authorToShow[i].style.display = "none"
-}respDiscTopic
-
-
-
 $(window).load(function () {
     getDiscussionGroupPosts()
 
 
-})   
+})
 
