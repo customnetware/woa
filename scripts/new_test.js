0257c98e61396675b4298aaaf584ce07d397ee48
Will Webb <willwebb@att.net>
2023-12-22 17:16:05 -0800

    UPDATE

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -205,6 +205,8 @@ function getProfilePage() {
     var profileID = regExp.exec(document.getElementById("HeaderPublishAuthProfile").href)[1].split(",")[0]
     $("#userProfile").load(pageLocation("/news/28118~792554/webmaster-only") + " #contentInner", function () {
         document.getElementById("userProfile").getElementsByClassName("clsPageMenu")[0].remove()
+        document.getElementById("userProfile").getElementsByClassName("clsHeader")[0].remove()
+
 
         $.get(pageLocation("/Member/28118~" + profileID), function () {
         }).done(function (responseText) {
