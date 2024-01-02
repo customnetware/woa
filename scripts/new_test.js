4d338d7d239f61afeb10d21f37245d88e2604ddf
Will Webb <willwebb@att.net>
2024-01-02 04:16:34 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -279,7 +279,7 @@ function showComments(SelectedPostID, postComment) {
             let id03 = "'" + frameLink.split(",")[5] + "'"
             alert(id01 + id02 + id03)
           /*  frameWindow.AV.EditorLauncher.discussionTopic(id01, id02, '', 'reply', 'Reply to Post', id03)*/
-            frameWindow.AV.EditorLauncher.discussionTopic('334766', '11315', '', 'reply', 'Reply to Post', 'lnkTopicReply334766')
+            frameWindow.document.AV.EditorLauncher.discussionTopic('334766', '11315', '', 'reply', 'Reply to Post', 'lnkTopicReply334766')
 
 
 
