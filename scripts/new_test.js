25720eb675f549708d3f80da569adb361e647ccb
Will Webb <willwebb@att.net>
2024-01-02 13:02:34 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -261,14 +261,14 @@ function getGroupPosts(selectedGroups, numOfDays) {
     }
 }
 function showComments(SelectedPostID, postComment) {
-
-    document.getElementById("saveComment").href = "javascript:showComments(" + SelectedPostID + ",true)"
-
     let selectedPost = document.getElementById("recentPostsBody").getElementsByTagName("p")[SelectedPostID]
     let frameLink = /\(([^)]+)\)/.exec(selectedPost.getElementsByTagName("a")[1].href)[1].replaceAll("'", "")
-    document.getElementById("woaFrame").src = "/Discussion/28118~" + frameLink.split(",")[1] + "~" + frameLink.split(",")[5].replace("lnkTopicReply", "")
-    $('#woaFrame').on('load', function () {
-        if (postComment == true) {
+
+
+    if (postComment == true) {
+        document.getElementById("woaFrame").src = "/Discussion/28118~" + frameLink.split(",")[1] + "~" + frameLink.split(",")[5].replace("lnkTopicReply", "")
+        $('#woaFrame').on('load', function () {
+
             try {
 
                 let frameWindow = document.getElementById('woaFrame').contentWindow
@@ -276,7 +276,7 @@ function showComments(SelectedPostID, postComment) {
                 let id01 = "'" + frameLink.split(",")[0] + "'"
                 let id02 = "'" + frameLink.split(",")[1] + "'"
                 let id03 = "'" + frameLink.split(",")[5] + "'"
-            
+
                 frameWindow.AV.EditorLauncher.discussionTopic(frameLink.split(",")[0], frameLink.split(",")[1], '', 'reply', 'Reply to Post', frameLink.split(",")[5])
 
 
@@ -290,13 +290,14 @@ function showComments(SelectedPostID, postComment) {
                 }, 1000)
             } catch (error) { alert(error.message) }
 
-        }
-    })
 
+        })
+    }
 
 
     if (selectedPost.childElementCount > 3) {
         document.getElementById("postComments").innerHTML = selectedPost.innerHTML
+        document.getElementById("saveComment").href = "javascript:showComments(" + SelectedPostID + ",true)"
 
         if (!$("#postSettingsAlert").is(":visible")) { $("#postSettingsAlert").modal("show") }
     }
