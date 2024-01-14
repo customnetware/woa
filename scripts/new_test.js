5db9c43dd6865b79d47e48c237389cdf7302fcbe
Will Webb <willwebb@att.net>
2024-01-14 14:35:48 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -406,13 +406,11 @@ function portalFormInput(selectedPostID, groupID) {
     commentForm = document.getElementById((selectedPostID !== "replyContent") ? selectedPostID.replace("post", "comment") : selectedPostID)
     setTimeout(function () {
         let portal = document.getElementById('woaFrame').contentWindow.document
-       
-        if (portal.getElementById("txt_post_body") !== null && commentForm.value !== "") {
-            let post_subject = portal.getElementsByClassName("x-form-text x-form-field form-items-container")
+        let post_subject = portal.getElementsByClassName("x-form-text x-form-field form-items-container")
 
+        if (portal.getElementById("txt_post_body") !== null && commentForm.value !== "") {
             portal.getElementById("txt_post_body").innerHTML = commentForm.value
-            if (post_subject.length > 0) {post_subject[0].value = commentForm.value.substring(0, 10+"...") }
-            
+            if (post_subject.length > 0) { post_subject[0].value = commentForm.value.substring(0, 10 + "...") }
             let waitForText = setInterval(function () {
                 if (portal.getElementById("txt_post_body").innerHTML == commentForm.value) {
                     clearInterval(waitForText)
