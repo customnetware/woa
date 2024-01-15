5d4fa14333459b7384267a5dddaa97b965cfd916
Will Webb <willwebb@att.net>
2024-01-14 17:18:42 -0800

    update

--- scripts/new_test.js
+++ scripts/new_test.js
@@ -407,14 +407,26 @@ function portalFormInput(selectedPostID, groupID) {
         commentForm = document.getElementById((selectedPostID !== "replyContent") ? selectedPostID.replace("post", "comment") : selectedPostID)
         let portal = document.getElementById('woaFrame').contentWindow.document
         let post_subject = portal.getElementsByClassName("x-form-text x-form-field form-items-container")
-        if (commentForm.value.length > 10) { portal.getElementById("txt_post_body").innerHTML = commentForm.value }
+
+
+        if (commentForm.value.length > 10 && portal.getElementById("txt_post_body") !== null) { portal.getElementById("txt_post_body").innerHTML = commentForm.value }
+
+
+
         if (post_subject.length > 0) { post_subject[0].value = commentForm.value.substring(0, 10) + "..." }
+
+
+
+
+
         if (portal.getElementById("txt_post_body") !== null && portal.getElementById("txt_post_body").value.length > 10 && commentForm.value.length > 10) {
             portal.getElementsByClassName(" x-btn-text save-button")[0].click()
             portalInputConfirm(selectedPostID, groupID)
         } else {
             portalFormInput(selectedPostID, groupID)
         }
+
+
     }, 500)
 }
 function portalInputConfirm(selectedPostID, groupID) {
