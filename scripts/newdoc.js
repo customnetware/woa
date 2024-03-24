2df2621ab2f05adc7950f6ec1f8f3951301f7919
customnetware@gmal.com <willwebb@att.net>
2024-03-24 13:09:13 -0700

    update

--- scripts/newdoc.js
+++ scripts/newdoc.js
@@ -1,56 +1,45 @@
+const isLocal = (window.location.hostname == "localhost") ? ".html" : ""
 function getPortalDocuments(docID) {
     let pageDocuments = document.getElementById("pageLinks")
     if (docID === "") { docID = "contentInner" }
-    $.get("/resourcecenter/28118/resource-center", function () { })
+    $.get("/resourcecenter/28118/resource-center" + isLocal, function () { })
         .done(function (responseText) {
-            let documents = new DOMParser().parseFromString(responseText, "text/html")
             while (pageDocuments.firstChild) { pageDocuments.removeChild(pageDocuments.firstChild) }
-            let allDocuments = documents.getElementsByClassName("clsTreeNde")
+            let documents = new DOMParser().parseFromString(responseText, "text/html")
+            let allDocuments = documents.getElementById(docID).getElementsByClassName("clsTreeNde")
             if (docID !== "contentInner") {
-                let testDiv = document.createElement("div")
-                let testA = document.createElement("a")
-                testA.href = "javascript:getPortalDocuments('" + documents.getElementById(docID).parentElement.parentElement.parentElement.id + "')"
-                testA.innerHTML = documents.getElementById(docID.replace("contents", "f")).innerText
+                let folderIcon = document.createElement("i")
+                let folderLink = document.createElement("a")
+                let lastFolder = document.createElement("span")
 
+                folderLink.href = "javascript:getPortalDocuments('" + documents.getElementById(docID).parentElement.parentElement.parentElement.id + "')"
+                folderLink.innerHTML = documents.getElementById(docID.replace("contents", "f")).innerText
+                folderIcon.className = "fa fa-folder-open-o fa-lg"
+                folderIcon.style.marginRight = "10px"
+                lastFolder.appendChild(folderIcon)
+                lastFolder.appendChild(folderLink)
+                pageDocuments.appendChild(lastFolder)
 
-                let testSpan = document.createElement("span")
-                let stackSpan = document.createElement("span")
-
-                stackSpan.className = "fa fa-folder-open-o fa-lg"
-                stackSpan.style.marginRight = "10px"
-
-
-
-                testSpan.innerText = " " + documents.getElementById(docID.replace("contents", "f")).innerText
-                testDiv.appendChild(stackSpan)
-                testDiv.appendChild(testA)
-
-                pageDocuments.appendChild(testDiv)
             }
-
-
             for (let d = 0; d < allDocuments.length; d++) {
                 let parentFolderID = allDocuments[d].parentElement.parentElement.parentElement.parentElement.id
-                if (parentFolderID !== "contents465149" && parentFolderID == docID) {
-                    let pageDocument = document.createElement("div")
-                    let pageSpan = document.createElement("span")
-                    let pageAnchor = document.createElement("a")
+               if (parentFolderID !== "contents465149" && parentFolderID == docID) {
 
-                    pageSpan.className = "fa fa-folder-o fa-lg"
-                    pageSpan.style.marginRight = "10px"
-                    pageAnchor.innerHTML = allDocuments[d].innerHTML
+                    let pageIcon = document.createElement("i")
+                    let pageLink = document.createElement("a")
+                    let pageDocument = document.createElement("span")
+
+                    pageIcon.className = "fa fa-folder-o fa-lg"
+                    pageIcon.style.marginRight = "10px"
+                    pageLink.innerHTML = allDocuments[d].innerHTML
                     if (allDocuments[d].id.charAt(0) == "f") {
-                        pageAnchor.href = "javascript:getPortalDocuments('" + allDocuments[d].id.replace("f", "contents") + "')"
-                    } else { pageSpan.className = "fa fa-file-o fa-lg" }
-
-
-                    pageDocument.appendChild(pageSpan)
-                    pageDocument.appendChild(pageAnchor)
+                        pageLink.href = "javascript:getPortalDocuments('" + allDocuments[d].id.replace("f", "contents") + "')"
+                    } else { pageIcon.className = "fa fa-file-o fa-lg" }
+                    pageDocument.appendChild(pageIcon)
+                    pageDocument.appendChild(pageLink)
                     pageDocuments.appendChild(pageDocument)
-
-
-                }
-            }
+                
+            }}
         })
 }
 $(window).load(function () {
