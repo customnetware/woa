62e24c188189c78b8db9acf32e35c3442efe19bb
customnetware@gmal.com <willwebb@att.net>
2024-04-16 14:34:43 -0700

    update

--- scripts/myWoodbridge.js
+++ scripts/myWoodbridge.js
@@ -238,7 +238,7 @@ try {
 function openMenu() {
     let menuList = document.getElementsByClassName("menu-sub-group")[1]
     let menuItems = menuList.children
-    /*  document.getElementsByClassName("menu-sub-group")[1].style = "left: 0px;"*/
+      document.getElementsByClassName("menu-sub-group")[1].style = "left: 0px;"
     for (let p = 0; p < menuItems.length; p++) { menuItems[p].style.display = "block" }
 }
 
